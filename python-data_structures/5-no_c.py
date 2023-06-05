114b39c3057befec24d03d05d01b6e3a3ef620f9
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-06-05 13:56:42 -0500

    add the file

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    return my_string.replace('c', '').replace('C', '')
+    new_string = ""
+    for char in my_string:
+        if char not in ['c', 'C']:
+            new_string += char
+    return new_string
