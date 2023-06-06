8300d4edbb066b47214fe93ff9040b969ba5af75
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-06-06 13:19:27 -0500

    add the file for the fix

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if len(sentence) < 0:
-        return None
-    else:
-        first_character = sentence[0]
+    first_character = sentence[0] if length > 0 else None
     return length, first_character
