a268059ddcd03aa4e5a927b324b0159f75873dc9
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-06-07 15:18:36 -0500

    add the file 2-uniq_add.py

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_integers = set()
+    total = 0
+    for i in my_list:
+        if i not in unique_integers:
+            unique_integers.add(i)
+            total += i
+    return total
