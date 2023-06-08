2e8abc7be7799fc6baf632b20760f6bb4fdfddf7
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-06-08 16:07:46 -0500

    add the README.md and the 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(x):
-        if i < len(my_list):
-            print(my_list[i])
+    try:
+        count = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return count
