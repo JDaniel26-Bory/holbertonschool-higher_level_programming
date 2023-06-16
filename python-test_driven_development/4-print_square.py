214fa9c226a1d2aaae8c88f66095ab7fb0ab79aa
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-06-16 11:56:29 -0500

    add the file

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -10,8 +10,7 @@ def print_square(size):
     """
     if type(size) is not int:
         raise TypeError("size must be an integer")
-    elif size < 0:
-        raise TypeError("size must be >= 0")
-
-    for _ in range(size):
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
         print("#" * size)
