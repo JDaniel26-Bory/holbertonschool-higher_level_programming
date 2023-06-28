83a7142f48ab7973b72517b7c5823671364bb61e
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-06-28 14:53:19 -0500

    update the class Rectangle and also the metjod def display

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,7 +79,7 @@ class Rectangle(Base):
         for _ in range(self.__y):
             print()
         for _ in range(self.__height):
-            print(" " * self.__width + "#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """Define str class"""
