03e4e57a53575fc5f3cbe78203d285abf8821a92
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-06-29 15:11:10 -0500

    update base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """Define Base Class"""
 
 import json
+import csv
 
 
 class Base:
@@ -25,3 +26,15 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation
+        of list_objs to a file"""
+        list_cls = []
+        if list_objs is not None:
+            for i in list_objs:
+                list_cls += [(cls.to_dictionary(i))]
+
+        with open(cls.__name__ + ".json", "w", encoding="utf-8") as w:
+            w.write(cls.to_json_string(list_cls))
