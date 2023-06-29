84f339329718b0ed30d70345b6b07d204246ac35
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-06-29 10:00:31 -0500

    update the file square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,6 +9,19 @@ class Square(Rectangle):
     """Define Square class"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+    
+    @property
+    def size(self):
+        """Size Getter"""
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        """Set the value in private instance attribute"""
+        self.width = value
+        self.height = value
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    
