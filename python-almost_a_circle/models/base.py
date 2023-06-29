410f317c6c5ad367fbf05d10d7831a4017f3ade9
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-06-29 10:39:51 -0500

    update base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define Base Class"""
 
+import json
 
 class Base:
     """Define Base Class"""
@@ -15,3 +16,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
