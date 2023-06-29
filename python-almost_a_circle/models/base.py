8962499fa41a94871544678944bf9b8b4feb0d67
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-06-29 10:46:10 -0500

    update base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -23,5 +23,4 @@ class Base:
         representation of list_dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
